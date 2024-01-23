14f6b1e6edfd31e4e9ab8472ac426417182be2b3
Gutsey68 <139963620+Gutsey68@users.noreply.github.com>
2024-01-23 10:38:05 +0100

    23/01/2024 11h

---
+++ _partial/footer.php
@@ -0,0 +1,16 @@
+<footer class="py-5 text-center text-body-secondary bg-body-tertiary">
+			<p>Créé par <a href="https://ce-formation.com/">CE FORMATION</a>.</p>
+			<p>
+				<a href="mentions.php">Mentions légales</a> | <a href="contact.php">Contact</a>
+			</p>
+			<p class="mb-0">
+				<a href="#">Back to top</a>
+			</p>
+		</footer>
+	<?php if($strTitle == "Blog") echo("<script>
+			changePeriod();
+		</script>")
+	
+	?>
+    </body>
+</html>
---
+++ _partial/nav.php
@@ -0,0 +1,10 @@
+
+
+<div class="nav-scroller py-1 mb-3 border-bottom">
+				<nav class="nav nav-underline justify-content-between">
+					<a class="nav-item nav-link link-body-emphasis <?php if($strTitle == "Acceuil") echo("active") ?>" href="index.php">Accueil</a>
+					<a class="nav-item nav-link link-body-emphasis <?php if($strTitle == "A propos") echo("active") ?>" href="about.php">A propos</a>
+					<a class="nav-item nav-link link-body-emphasis <?php if($strTitle == "Blog") echo("active") ?>" href="blog.php">Blog</a>
+					<a class="nav-item nav-link link-body-emphasis <?php if($strTitle == "Contact") echo("active") ?>" href="contact.php">Contact</a>
+				</nav>
+			</div>
